f421317053a663038d4ec6f9055772bd20b0f7d0
Salvador Salgado <72042153+Chava27@users.noreply.github.com>
2022-08-18 17:09:32 -0500

    Update praseTable.js

--- Redes/Web-App/estudiantes/src/utils/praseTable.js
+++ Redes/Web-App/estudiantes/src/utils/praseTable.js
@@ -27,6 +27,50 @@ const praseDate = (date) => {
 };
 
 const praseGrade = (grade) => {
-	return "A";
+	let grade_us = ""
+	if (grade < 64){
+		grade_us= "E";
+		return grade_us;
+	}
+	else if (grade<67){
+		grade_us="D";
+		return grade_us;
+	}
+	else if (grade<70){
+		grade_us="D+";
+		return grade_us;
+	}
+	else if (grade<73){
+		grade_us="C-";
+		return grade_us;
+	}
+	else if (grade<77){
+		grade_us="C";
+		return grade_us;
+	}
+	else if (grade<80){
+		grade_us="C+";
+		return grade_us;
+	}
+	else if (grade<83){
+		grade_us="B-";
+		return grade_us;
+	}
+	else if (grade<87){
+		grade_us="B";
+		return grade_us;
+	}
+	else if (grade<90){
+		grade_us="B+";
+		return grade_us;
+	}
+	else if (grade<93){
+		grade_us="A-";
+		return grade_us;
+	}
+	else{
+		grade_us="A"
+		return grade_us;
+	}
 };
 export default praseTable;
