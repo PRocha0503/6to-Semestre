ad0f7a5d8a8af8463401b9b279fb4cb050b14b8d
Miguel Arriaga Velasco <marriagav@Miguels-MacBook-Pro.local>
2022-08-25 17:14:33 -0500

    implemented textbook multiplication

--- Algoritmos/Tareas/strassen/main.cpp
+++ Algoritmos/Tareas/strassen/main.cpp
@@ -6,8 +6,12 @@ using namespace std;
 int main(){
     // Initialize the matrix controller
     MatrixOperations matrixController = MatrixOperations("01.Matrix_A_16_2_4.csv","02.Matrix_B_16_2_4.csv");
+    // MatrixOperations matrixController = MatrixOperations("test1.csv","test2.csv");
     // Print the two matrices
     matrixController.printMatrix(matrixController.getFirstMatrix());
     matrixController.printMatrix(matrixController.getSecondMatrix());
-    // TODO: multiply them with both methods
+    // Multiplication with the textbook method
+    vector<vector<int> > resultFromTextBook = matrixController.textBookMultiplication();
+    matrixController.printMatrix(resultFromTextBook);
+    // TODO: multiply them with Strassen method
 }
