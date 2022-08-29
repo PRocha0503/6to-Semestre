2149aa2ab190d9aefc2f20db64fdf31ca68a49cd
Miguel Arriaga Velasco <marriagav3@gmail.com>
2022-08-29 12:23:41 -0500

    added names

--- Algoritmos/Tareas/strassen/matrixOperations.h
+++ Algoritmos/Tareas/strassen/matrixOperations.h
@@ -1,3 +1,11 @@
+/*
+HW 03 - Strassen's Algorithm
+Patricio Bosques Rosas: A01781663
+Pablo Rocha Ojeda: A01028638
+Luis Javier Karam Galland: A01751941
+Miguel Arriaga Velasco: A01028570
+*/
+
 #ifndef MATRIXOPERATIONS_H
 #define MATRIXOPERATIONS_H
 
