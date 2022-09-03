1ce11c21dc9f744a271c6a925be7fc857f3d70c4
Stephan Guingor <stephan.guingor04@gmail.com>
2022-09-03 14:49:51 -0500

    Add carbon design library
    - configure sass
    - ts for library auto completion

--- Redes/Proyect/frontend/next.config.js
+++ Redes/Proyect/frontend/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  sassOptions: {
+    includePaths: [__dirname + 'styles', "node_modules"],
+  },
 }
 
 module.exports = nextConfig
---
+++ Redes/Proyect/frontend/pages/login.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import { Button } from "carbon-components-react";
+
+const Login: NextPage = () => {
+  return (
+    <div className='login-container'>
+        <Button kind='secondary'>Login</Button>
+        
+    </div>
+  )
+}
+
+export default Login;
