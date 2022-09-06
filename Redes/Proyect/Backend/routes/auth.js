e7501bf997214223e6e75d5421690b52ba60e287
Pablo Rocha <pablorochatmm@gmail.com>
2022-09-05 20:22:24 -0500

    cors

--- Redes/Proyect/Backend/routes/auth.js
+++ Redes/Proyect/Backend/routes/auth.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const User = require("../models/user");
-const { login } = require("../controllers/auth.js");
+const { login, logged } = require("../controllers/auth.js");
+const { validateJWT } = require("../middleware");
 
 const router = Router();
 
 router.post("/login", login);
+router.get("/logged", [validateJWT], logged);
 module.exports = router;
