ebcb7029c1cc81024928aeafa7cfaf51ed701c13
Stephan Guingor <stephan.guingor04@gmail.com>
2022-09-08 18:53:44 -0500

    Feat: LogTab added

--- Redes/Proyect/frontend/pages/index.tsx
+++ Redes/Proyect/frontend/pages/index.tsx
@@ -7,17 +7,24 @@ import { useState, useEffect } from "react";
 import { useUser } from "../components/user";
 import { useRouter } from "next/router";
 import DocumentPreview from "../components/DocumentPreview";
+import LogTab from "@/components/LogTab";
 
 const Home: NextPage = () => {
 	const user = useUser();
 
 	const [selectedNode, setSelectedNode] = useState<any>(null);
-
+	const logs = [
+		{ date: new Date(200), user: { username: "test" } },
+		{ date: new Date(), user: { username: "a" } },
+		{ date: new Date(), user: { username: "b" } },
+		{ date: new Date(), user: { username: "test" } },
+	]
 	return (
 		<div className={styles.container}>
 			<div className={styles.contentLayout}>
 				<FolderExplorer onSelect={(node) => setSelectedNode(node)} />
 				<div>{selectedNode && <DocumentPreview document={selectedNode} />}</div>
+				<LogTab logs={logs}></LogTab>
 			</div>
 		</div>
 	);
