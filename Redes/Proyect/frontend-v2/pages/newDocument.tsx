0f44b83cfbdc8649f0c2f6c7aea9c099bb33c394
Luisjakg <luisjakg@gmail.com>
2022-10-03 20:01:21 -0500

    newDocument Fix

--- Redes/Proyect/frontend-v2/pages/newDocument.tsx
+++ Redes/Proyect/frontend-v2/pages/newDocument.tsx
@@ -5,6 +5,7 @@ import {
 	Icon,
 	IconSize,
 	InputGroup,
+	FileInput,
 	Intent,
 	Menu,
 	MenuItem,
@@ -12,59 +13,109 @@ import {
 	Switch,
 	Tag,
 	Label,
+	AnchorButton,
 } from "@blueprintjs/core";
+
+import { DateInput } from "@blueprintjs/datetime";
+
 
 import styles from "../styles/newDocument.module.css";
 
 const NewDocument: NextPage = () => {
 	return (
 		<div>
-			<h1 className="bp4-heading">H1 heading</h1>
-			<div className={styles.textInput}>
-				<Label as="h1">Titulo del documento</Label>
+			<h1 className="bp4-heading">Subir Archivo	</h1>
+			<div className={styles.textInputs}>
+				<Label as="h1">Los apartados marcados con * son obligatorios</Label>
+
+				<div className={styles.textInput}>
+					<FileInput
+						disabled={false} 
+						text="Escoger archivo..."  
+						typeof="file.pdf"
+					/>
+				</div>
+
+				<div className={styles.textInput}>
 				<InputGroup
 					large={true}
 					fill={false}
 					type="text"
 					leftElement={<Icon icon="document" />}
 					// onChange={this.handleTagChange}
-					placeholder="Titulo"
+					placeholder="Titulo*"
 					// rightElement={resultsTag}
 					// small={true}
 					// value={tagValue}
 				/>
+				</div>
+
+				<div className={styles.textInput}>
 				<InputGroup
 					large={true}
 					fill={false}
 					type="text"
 					leftElement={<Icon icon="folder-close" />}
 					// onChange={this.handleTagChange}
-					placeholder="Expediente"
+					placeholder="Expediente*"
 					// rightElement={resultsTag}
 					// small={true}
 					// value={tagValue}
 				/>
+				</div>
+
+				<div className={styles.textInput}>
 				<InputGroup
 					large={true}
 					fill={false}
 					type="folio"
 					leftElement={<Icon icon="numerical" />}
-					placeholder="Folio"
+					placeholder="Folio*"
 				/>
+				</div>
+
+				<div className={styles.textInput}>
 				<InputGroup
+					//TODO cambiar el tipo de input a DateInput2
 					large={true}
 					fill={false}
-					type="folio"
-					leftElement={<Icon icon="calendar" />}
-					placeholder="Fecha de creacion"
+					type="text"
+					leftElement={<Icon icon="folder-close" />}
+					// onChange={this.handleTagChange}
+					placeholder="Fecha*"
+					// rightElement={resultsTag}
+					// small={true}
+					// value={tagValue}
 				/>
+				</div>
+
+				<div className={styles.textInput}>
+				<DateInput
+    				formatDate={date => date.toLocaleString()}
+    				parseDate={str => new Date(str)}
+    				placeholder="M/D/YYYY"
+				/>
+
+				</div>
+
+				<div className={styles.textInput}>
 				<InputGroup
 					large={true}
 					fill={false}
 					type="text"
 					leftElement={<Icon icon="tag" />}
-					placeholder="Etiquetas"
+					placeholder="Etiquetas*"
 				/>
+				</div>
+
+				<div className={styles.textInput}>
+				<AnchorButton 
+					text = "Subir" 
+					rightIcon="upload"
+					fill = {true}
+					intent = "primary"
+				/>
+				</div>
 			</div>
 		</div>
 	);
