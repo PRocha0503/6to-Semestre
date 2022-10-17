a101461aa2964f324275c7b55734b1969d69cf03
Miguel Arriaga Velasco <marriagav3@gmail.com>
2022-10-17 14:04:49 -0500

    added names

--- Algoritmos/Tareas/k-sat/KLiteral.h
+++ Algoritmos/Tareas/k-sat/KLiteral.h
@@ -1,3 +1,11 @@
+/*
+KSAT Shoning
+Patricio Bosques Rosas: A01781663
+Pablo Rocha Ojeda: A01028638
+Luis Javier Karam Galland: A01751941
+Miguel Arriaga Velasco: A01028570
+*/
+
 #pragma once
 
 class KLiteral{
