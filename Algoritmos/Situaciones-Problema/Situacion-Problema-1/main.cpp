81b8b47a3c7aee253ad1db5b4ea502a6557a9124
Miguel Arriaga Velasco <marriagav3@gmail.com>
2022-11-07 11:41:48 -0600

    reversed code search now working

--- Algoritmos/Situaciones-Problema/Situacion-Problema-1/main.cpp
+++ Algoritmos/Situaciones-Problema/Situacion-Problema-1/main.cpp
@@ -1,12 +1,14 @@
 /*
-ZFunction with KMP
-Patricio Bosques Rosas: A01781663
+Situación Integradora 1
+Patricio Bosque Rosas: A01781663
 Pablo Rocha Ojeda: A01028638
 Luis Javier Karam Galland: A01751941
 Miguel Arriaga Velasco: A01028570
 */
 
 #include "ZFunction.cpp"
+#include <thread>
+#include <future>
 
 void printVectorToFile(vector<vector<int>> v, string fileName){
     ofstream file;
@@ -15,24 +17,84 @@ void printVectorToFile(vector<vector<int>> v, string fileName){
         cout << "Error opening file" << endl;
         return;
     }
-    file<<"Number of times the malicious code is in the file: "<<v.size()<<endl;
-    for (int i = 0; i < v.size(); i++){
-        file<<"\nString found in:"<<endl;
-        file <<"Starting Position: " << v[i][0] << " Ending Position: " << v[i][1] << endl;
+    if (v.size() == 0){
+        file << "The falicious code was not found in the file" << endl;
+        return;
+    }
+
+    file<<"Number of times the non reversed malicious code is in the file: "<<v[0][0]<<endl;
+    file<<"Number of times the reversed malicious code is in the file: "<<v[0][1]<<endl;
+    file<<"Total number of times the malicious code is in the file: "<<v[0][2]<<endl;
+
+    for (int i = 1; i < v.size(); i++){
+        if (v[i][2] == 1){
+            file<<"\nReversed malicious code found in:"<<endl;
+            file <<"Starting Position: " << v[i][0] << " Ending Position: " << v[i][1] << endl;
+        }
+        else{
+            file<<"\nMalicious code found in:"<<endl;
+            file <<"Starting Position: " << v[i][0] << " Ending Position: " << v[i][1] << endl;
+        }
+        
     }
     file.close();
     cout<<"Results written successfully in file: "<<fileName<<endl;
 }
 
 void printVector(vector<vector<int>> v){
-    for (int i = 0; i < v.size(); i++){
-        cout<<"\nString found in:"<<endl;
-        cout <<"Starting Position: " << v[i][0] << " Ending Position: " << v[i][1] << endl;
+
+    if (v.size() == 0){
+        cout << "The falicious code was not found in the file" << endl;
+        return;
+    }
+
+    cout<<"Number of times the non reversed malicious code is in the file: "<<v[0][0]<<endl;
+    cout<<"Number of times the reversed malicious code is in the file: "<<v[0][1]<<endl;
+    cout<<"Total number of times the malicious code is in the file: "<<v[0][2]<<endl;
+
+    for (int i = 1; i < v.size(); i++){
+        if (v[i][2] == 1){
+            cout<<"\nReversed malicious code found in:"<<endl;
+            cout<<"Starting Position: " << v[i][0] << " Ending Position: " << v[i][1] << endl;
+        }
+        else{
+            cout<<"\nMalicious code found in:"<<endl;
+            cout <<"Starting Position: " << v[i][0] << " Ending Position: " << v[i][1] << endl;
+        }
     }
     cout << endl;
 }
 
 int main(){
+    /*
+    ZFunction zFunctionQ("Tests/quickTransmissionTest.txt","Tests/quickTest.txt");
+    ZFunction zFunction1("Tests/transmission.txt","Tests/mcode1.txt");
+    ZFunction zFunction2("Tests/transmission.txt", "Tests/mcode2.txt");
+    ZFunction zFunction3("Tests/transmission.txt", "Tests/mcode3.txt");
+
+    auto futureQ = async(zFunctionQ.zFunctionKMP());
+    auto future1 = async(zFunction1.zFunctionKMP());
+    auto future2 = async(zFunction2.zFunctionKMP());
+    auto future3 = async(zFunction3.zFunctionKMP());
+
+    vector<vector<int>> resultQ = futureQ.get();
+    vector<vector<int>> v1 = future1.get();
+    vector<vector<int>> v2 = future2.get();
+    vector<vector<int>> v3 = future3.get();
+
+    printVector(resultQ, "Tests/quickTest-Result.txt");
+    printVectorToFile(v1, "Tests/mcode1-Result.txt");
+    printVectorToFile(v2, "Tests/mcode2-Result.txt");
+    printVectorToFile(v3, "Tests/mcode3-Result.txt");
+
+    return 0;
+    */
+    
+
+    //Quick test
+    ZFunction zFunctionQ("Tests/quickTransmissionTest.txt","Tests/quickTest.txt");
+    vector<vector<int>>zFunctionResultQ= zFunctionQ.zFunctionKMP();
+    printVectorToFile(zFunctionResultQ, "Tests/quickTest-Result.txt");
 
     //First file
     ZFunction zFunction("Tests/transmission.txt","Tests/mcode1.txt");
@@ -44,7 +106,6 @@ int main(){
     vector<vector<int>> zFunctionResult2 = zFunction2.zFunctionKMP();
     printVectorToFile(zFunctionResult2, "Tests/mcode2-Result.txt");
     
-
     //Third file
     ZFunction zFunction3("Tests/transmission.txt", "Tests/mcode3.txt");
     vector<vector<int>> zFunctionResult3 = zFunction3.zFunctionKMP();
